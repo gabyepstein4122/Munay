84c7054dcb7ed4f847949200650168b24bd25984
Gabriel Epstein <gabrielepstein4122@gmail.com>
2023-06-12 17:47:08 -0300

    subida al dominio

--- boton.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navbar = document.querySelector('.navbar');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  navbar.classList.toggle('active');
-  mobileMenu.classList.toggle('active');
-});
